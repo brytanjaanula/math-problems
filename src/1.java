a4ddbe863b458962b7f6527799109b553fdbb6dd
brytanjaanula <brytanjaanula@gmail.com>
2025-03-06 07:33:00 +0000

    Add 1.java

---
+++ src/1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Random r = new Random();
+        int x = r.nextInt(10);
+        int y = r.nextInt(10);
+        System.out.println(x + " + " + y + " = ");
+    }
+}
