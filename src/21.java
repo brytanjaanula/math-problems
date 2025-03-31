31152dd2d66f767952f33302535a87558b1b074b
brytanjaanula <brytanjaanula@gmail.com>
2025-03-31 06:20:46 +0000

    Add 21.java

---
+++ src/21.java
@@ -0,0 +1,9 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example code to calculate the area of a rectangle
+        double length = 10;
+        double width = 5;
+        double area = length * width;
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
