5289152cdd815c0ce0925b3f662c7b23458f922c
brytanjaanula <brytanjaanula@gmail.com>
2025-04-07 05:10:47 +0000

    Add 27.java

---
+++ src/27.java
@@ -0,0 +1,19 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example: Addition
+        int result = 5 + 3;
+        System.out.println("The sum of 5 and 3 is " + result);
+
+        // Example: Subtraction
+        int difference = 8 - 2;
+        System.out.println("The difference between 8 and 2 is " + difference);
+
+        // Example: Multiplication
+        int product = 7 * 6;
+        System.out.println("The product of 7 and 6 is " + product);
+
+        // Example: Division
+        int quotient = 10 / 3; // Equivalent to 10 divided by 3
+        System.out.println("The division result of 10 and 3 is " + quotient);
+    }
+}
