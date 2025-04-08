8a10271bd8ecea986cae3a291e62392b2c5b56f9
brytanjaanula <brytanjaanula@gmail.com>
2025-04-08 08:25:34 +0000

    Add 29.java

---
+++ src/29.java
@@ -0,0 +1,8 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example code for solving basic arithmetic operations
+        System.out.println("2 + 3 = " + (2 + 3));
+        System.out.println("5 - 2 = " + (5 - 2));
+        System.out.println("7 * 4 = " + (7 * 4));
+    }
+}
