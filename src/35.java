78a0656064e71dc2fbe2dc0632c80f04f285fbec
brytanjaanula <brytanjaanula@gmail.com>
2025-04-25 20:53:23 +0000

    Add 35.java

---
+++ src/35.java
@@ -0,0 +1,8 @@
+public class MathProblemSolver {
+    public static void main(String[] args) {
+        // Example Java code for solving math problems
+        System.out.println("Calculating 2 * 3 + 4 - 5");
+        double result = 2 * 3 + 4 - 5;
+        System.out.println("Result: " + result);
+    }
+}
