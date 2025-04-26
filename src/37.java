45ebef900ccc148c237bc5a66637a73b2a057d82
brytanjaanula <brytanjaanula@gmail.com>
2025-04-26 11:25:48 +0000

    Add 37.java

---
+++ src/37.java
@@ -0,0 +1,8 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        int x = 10;
+        double y = 2.5;
+
+        System.out.println("The sum of " + x + " and " + y + " is: " + (x + y));
+    }
+}
