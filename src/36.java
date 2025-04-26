e6d4c8f1ec11e602b59e23b8ae290e2fc0fc50c1
brytanjaanula <brytanjaanula@gmail.com>
2025-04-26 11:12:24 +0000

    Add 36.java

---
+++ src/36.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example problem: Summing numbers from 1 to 50
+        int sum = 0;
+        for (int i = 1; i <= 50; i++) {
+            sum += i;
+        }
+        System.out.println("The sum of numbers from 1 to 50 is: " + sum);
+    }
+}
