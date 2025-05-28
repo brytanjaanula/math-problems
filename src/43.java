7cf03a155f702868ed683500e0b1663e351397c9
brytanjaanula <brytanjaanula@gmail.com>
2025-05-28 00:36:01 +0000

    Add 43.java

---
+++ src/43.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class MathProblems {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4};
+        System.out.println("Array elements: " + Arrays.toString(array));
+    }
+}
